369bb95614227ebf005b882db731c8cfd33b3f80
Soyda <stevendruesne@gmail.com>
2021-11-23 17:51:43 +0100

    Managed to get datas from imdb

--- allocineScraping/allocineScraping/spiders/movieSpider.py
+++ allocineScraping/allocineScraping/spiders/movieSpider.py
@@ -9,7 +9,7 @@ class MoviespiderSpider(CrawlSpider):
     start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths = '//td[@class="titleColumn"]/a/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths = '//td[@class="titleColumn"]'), callback='parse_item', follow=True),
         # Rule(LinkExtractor(restrict_xpaths= '(//a[@class="button-right"])[2]'))
     )
 
@@ -21,7 +21,17 @@ class MoviespiderSpider(CrawlSpider):
 
         item = {}
 
-        item['movie_title'] = response.xpath('//h1[@class="TitleHeader__TitleText-sc-1wu6n3d-0 dxSWFG"]').get()
+        item['title'] = response.xpath('//h1/text()').get()
+        item['original_title'] = response.xpath('//div[@data-testid="hero-title-block__original-title"]/text()').get()
+        item['score'] = response.xpath('//div[@data-testid="hero-rating-bar__aggregate-rating__score"]/span/text()').get()
+        item['type'] = response.xpath('//a[@class="GenresAndPlot__GenreChip-cum89p-3 fzmeux ipc-chip ipc-chip--on-baseAlt"]/span/text()').getall()
+        item['release_date'] = response.xpath('//span[@class="TitleBlockMetaData__ListItemText-sc-12ein40-2 jedhex"]/text()')[0].get()
+        item['duration'] = "".join(response.xpath('//li[@data-testid="title-techspec_runtime"]/div[@class="ipc-metadata-list-item__content-container"]/text()').getall())
+        # item['resume'] = response.xpath('//div[@data-testid="hero-title-block__original-title"]/text()').get()
+        item['top_cast'] = response.xpath('//a[@data-testid="title-cast-item__actor"]/text()').getall()
+        item['audience'] = response.xpath('//span[@class="TitleBlockMetaData__ListItemText-sc-12ein40-2 jedhex"]/text()')[1].get()
+        # item['country'] = response.xpath('//div[@data-testid="hero-title-block__original-title"]/text()').get()
+        # item['original_language'] = response.xpath('//div[@data-testid="hero-title-block__original-title"]/text()').get()
 
         return item
 
